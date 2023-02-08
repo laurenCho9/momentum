b9c8bccd26892789075e3eed10b0411bf34460a5
laurenCho9 <lauren.choux@gmail.com>
2023-02-09 00:56:02 +0900

    login done

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const play = {
-  name: "hy",
-  coin: 10,
-  fat: true,
-};
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-console.log(play);
-play.lastName = "pika";
-play.coin = 20;
-console.log(play);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
